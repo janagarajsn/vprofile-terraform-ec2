b9ac6634eeb2d4e689cf1422955dd9bd4d45e0e6
Janagaraj Sangamanarayanan <janagarajs@gmail.com>
2024-08-23 12:21:04 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "vprofile-tfstate-ec2-us2 "
+    bucket  = "vprofile-tfstate-ec2-us2"
     key     = "tf-state"
     region  = "us-east-2"
     encrypt = true

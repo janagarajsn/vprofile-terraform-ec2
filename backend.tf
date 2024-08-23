10862bbf60eed8f0a473de30f64361512eaf4957
Janagaraj Sangamanarayanan <janagarajs@gmail.com>
2024-08-23 12:23:35 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "vprofile-tfstate-ec2-us2"
-    key     = "tf-state"
+    key     = "tf-state/terraform.tfstate"
     region  = "us-east-2"
     encrypt = true
     #dynamodb_table = "terraform-locks"  # Optional for state locking

3b20d6907db81105c305caa993fed25356a03232
Janagaraj Sangamanarayanan <janagarajs@gmail.com>
2024-08-23 12:15:57 -0500

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "vprofile-tfstate-ec2"
+    key            = "tf-state"
+    region         = "us-east-2"
+    encrypt        = true
+    #dynamodb_table = "terraform-locks"  # Optional for state locking
+  }
+}
